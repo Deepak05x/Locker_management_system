ec731db7bb12675b994e1bbda01ab23c778454ec
Deepak <hdeepak5902@gmail.com>
2024-10-24 23:00:07 +0530

    Reset Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 const Login = lazy(() => import("./components/Login"));
-const Signup = lazy(() => import("./components/Signup"));
+const Home = lazy(() => import("./components/Home"));
+const EnterEmail = lazy(() => import("./components/EnterEmail"));
+const EnterOtp = lazy(() => import("./components/EnterOtp"));
+const ResetPassword = lazy(() => import("./components/ResetPassword"));
 
 const App = () => {
     return (
         <Suspense fallback={<></>}>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/enter" element={<EnterEmail />} />
+                <Route path="/otp" element={<EnterOtp />} />
+                <Route path="/reset" element={<ResetPassword />} />
             </Routes>
         </Suspense>
     );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { PiLockersFill } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <section className="w-full bg-white drop-shadow-xl text-black px-12 py-6">
-            <PiLockersFill className="text-blue text-[3rem]" />
+            <Link to="/">
+                <PiLockersFill className="text-blue text-[3rem]" />
+            </Link>
         </section>
     );
 };
