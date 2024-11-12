d470f4eee266fb8dad81124430d3ffb5b6111db2
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-11-12 19:25:08 +0530

    changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const verifyToken=require('../utils/verifyUser.js')
 const router = express.Router();
 const { addStaff,removeStaff,editStaff,viewStaffDetails,viewAllStaff, addLocker, addMultipleLocker } = require('../controllers/adminControllers.js');
 
-router.post('/addStaff', addStaff);
-router.post('/removeStaff', removeStaff);
-router.put('/editStaff', editStaff);
-router.get('/viewAllStaff', viewAllStaff);
-router.post('/viewStaffDetails', viewStaffDetails);
-router.post('/addSingleLocker', addLocker);
-router.post('/addMultipleLocker', addMultipleLocker);
+router.post('/addStaff', verifyToken,addStaff);
+router.post('/removeStaff',verifyToken, removeStaff);
+router.put('/editStaff', verifyToken,editStaff);
+router.get('/viewAllStaff', verifyToken,viewAllStaff);
+router.post('/viewStaffDetails',verifyToken, viewStaffDetails);
+router.post('/addSingleLocker', verifyToken,addLocker);
+router.post('/addMultipleLocker',verifyToken, addMultipleLocker);
 
 module.exports = router;
